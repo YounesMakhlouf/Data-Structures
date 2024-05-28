e1711b30b65ee23600bc1e483a32df8f0e68d058
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2024-05-28 21:44:14 +0100

    initial commit

--- AVL/AVL.c
+++ AVL/AVL.c
@@ -1,135 +1,135 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define MAX(a, b) (((a)>(b))?(a):(b))
-#define MIN(a, b) (((a)<(b))?(a):(b))
-
-typedef struct noeud {
-    int val;
-    struct noeud *left;
-    struct noeud *right;
-    int balance;
-} node;
-
-typedef node *AVL;
-
-//AVL AVLInit() {
-//    return NULL;
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//#define MAX(a, b) (((a)>(b))?(a):(b))
+//#define MIN(a, b) (((a)<(b))?(a):(b))
+//
+//typedef struct noeud {
+//    int val;
+//    struct noeud *left;
+//    struct noeud *right;
+//    int balance;
+//} Node;
+//
+//typedef Node *AVL;
+//
+////AVL AVLInit() {
+////    return NULL;
+////}
+//
+//AVL AVLInit(int v) {
+//    AVL x = malloc(sizeof(Node));
+//    x->val = v;
+//    x->balance = 0;
+//    x->right = NULL;
+//    x->left = NULL;
+//    return x;
 //}
-
-AVL AVLInit(int v) {
-    AVL x = malloc(sizeof(node));
-    x->val = v;
-    x->balance = 0;
-    x->right = NULL;
-    x->left = NULL;
-    return x;
-}
-
-AVL find(int x, AVL A) {
-    if (A == NULL) return NULL;
-    if (A->val == x) return A;
-    if (A->val > x)
-        return find(x, A->left);
-    else
-        return find(x, A->right);
-}
-
-AVL RightRotation(AVL x) {
-    AVL y = x->left;
-    x->balance = x->balance - 1 - MAX(0, y->balance);
-    y->balance = y->balance - 1 + MIN(0, x->balance);
-    x->left = y->right;
-    y->right = x;
-    return y;
-}
-
-AVL LeftRotation(AVL x) {
-    AVL y = x->right;
-    x->balance = x->balance + 1 - MIN(0, y->balance);
-    y->balance = y->balance + 1 + MAX(0, x->balance);
-    x->right = y->left;
-    y->left = x;
-    return y;
-}
-
-AVL Reequilibrer(AVL a) {
-    if (a->balance > 0) {
-        if ((a->left)->balance > 0)
-            return LeftRotation(a);
-        else {
-            a->left = LeftRotation(a->left);
-            return RightRotation(a);
-        }
-    } else {
-        if ((a->right)->balance < 0)
-            return RightRotation(a);
-        else {
-            a->right = RightRotation(a->right);
-            return LeftRotation(a);
-        }
-    }
-}
-
-AVL insertion(AVL a, int val, int *modif) {
-    if (a == NULL) {
-        AVL x = AVLInit(val);
-        *modif = 1;
-        return x;
-    }
-    if (val > a->val) {
-        a->right = insertion(a->right, val, modif);
-        if (modif)
-            a->balance -= 1;
-    } else {
-        a->left = insertion(a->left, val, modif);
-        if (modif)
-            a->balance += 1;
-    }
-    if (abs(a->balance) > 1)
-        a = Reequilibrer(a);
-    if (a->balance == 0)
-        *modif = 0;
-    return a;
-}
-
-AVL deleteChild(AVL a, int *heritier, int *modif) {
-    if (a->right != NULL) {
-        a->right = deleteChild(a->right, heritier, modif);
-        if (*modif)
-            a->balance++;
-    } else {
-        *heritier = a->val;
-        *modif = 1;
-        return a->left;
-    }
-    if (abs(a->balance) > 1) a = Reequilibrer(a);
-    if (a->balance == 0) modif = 0;
-}
-
-AVL delete(AVL a, int val, int *modif) {
-    if (a == NULL) return NULL;
-    if (a->val > val) {
-        a->left = delete(a->left, val, modif);
-        if (*modif)
-            a->balance--;
-    }
-    if (a->val < val) {
-        a->right = delete(a->right, val, modif);
-        if (*modif)
-            a->balance++;
-    } else {
-        if (a->left == NULL) {
-            AVL r = a->right;
-            free(a);
-            *modif = 1;
-            return r;
-        } else {
-            int h;
-            a->left = deleteChild(a->left, &h, modif);
-            a->val = h;
-        }
-    }
-    if (abs(a->balance) > 1) a = Reequilibrer(a);
-    if (a->balance == 0) modif = 0;
-}
+//
+//AVL find(int x, AVL A) {
+//    if (A == NULL) return NULL;
+//    if (A->val == x) return A;
+//    if (A->val > x)
+//        return find(x, A->left);
+//    else
+//        return find(x, A->right);
+//}
+//
+//AVL RightRotation(AVL x) {
+//    AVL y = x->left;
+//    x->balance = x->balance - 1 - MAX(0, y->balance);
+//    y->balance = y->balance - 1 + MIN(0, x->balance);
+//    x->left = y->right;
+//    y->right = x;
+//    return y;
+//}
+//
+//AVL LeftRotation(AVL x) {
+//    AVL y = x->right;
+//    x->balance = x->balance + 1 - MIN(0, y->balance);
+//    y->balance = y->balance + 1 + MAX(0, x->balance);
+//    x->right = y->left;
+//    y->left = x;
+//    return y;
+//}
+//
+//AVL Reequilibrer(AVL a) {
+//    if (a->balance > 0) {
+//        if ((a->left)->balance > 0)
+//            return LeftRotation(a);
+//        else {
+//            a->left = LeftRotation(a->left);
+//            return RightRotation(a);
+//        }
+//    } else {
+//        if ((a->right)->balance < 0)
+//            return RightRotation(a);
+//        else {
+//            a->right = RightRotation(a->right);
+//            return LeftRotation(a);
+//        }
+//    }
+//}
+//
+//AVL insertion(AVL a, int val, int *modif) {
+//    if (a == NULL) {
+//        AVL x = AVLInit(val);
+//        *modif = 1;
+//        return x;
+//    }
+//    if (val > a->val) {
+//        a->right = insertion(a->right, val, modif);
+//        if (modif)
+//            a->balance -= 1;
+//    } else {
+//        a->left = insertion(a->left, val, modif);
+//        if (modif)
+//            a->balance += 1;
+//    }
+//    if (abs(a->balance) > 1)
+//        a = Reequilibrer(a);
+//    if (a->balance == 0)
+//        *modif = 0;
+//    return a;
+//}
+//
+//AVL deleteChild(AVL a, int *heritier, int *modif) {
+//    if (a->right != NULL) {
+//        a->right = deleteChild(a->right, heritier, modif);
+//        if (*modif)
+//            a->balance++;
+//    } else {
+//        *heritier = a->val;
+//        *modif = 1;
+//        return a->left;
+//    }
+//    if (abs(a->balance) > 1) a = Reequilibrer(a);
+//    if (a->balance == 0) modif = 0;
+//}
+//
+//AVL delete(AVL a, int val, int *modif) {
+//    if (a == NULL) return NULL;
+//    if (a->val > val) {
+//        a->left = delete(a->left, val, modif);
+//        if (*modif)
+//            a->balance--;
+//    }
+//    if (a->val < val) {
+//        a->right = delete(a->right, val, modif);
+//        if (*modif)
+//            a->balance++;
+//    } else {
+//        if (a->left == NULL) {
+//            AVL r = a->right;
+//            free(a);
+//            *modif = 1;
+//            return r;
+//        } else {
+//            int h;
+//            a->left = deleteChild(a->left, &h, modif);
+//            a->val = h;
+//        }
+//    }
+//    if (abs(a->balance) > 1) a = Reequilibrer(a);
+//    if (a->balance == 0) modif = 0;
+//}
--- B-Tree/b_tree.c
+++ B-Tree/b_tree.c
@@ -24,24 +24,39 @@ B_tree create_B_tree() {
     return x;
 }
 
+void print_B_tree(B_tree x, int level) {
+    if (x != NULL) {
+        int i;
+        for (i = 0; i < x->number_keys; i++) {
+            if (x->children[i] != NULL) {
+                print_B_tree(x->children[i], level + 1);
+            }
+            for (int j = 0; j < level; j++) printf("  ");
+            printf("%d\n", x->keys[i]);
+        }
+        if (x->children[i] != NULL) {
+            print_B_tree(x->children[i], level + 1);
+        }
+    }
+}
+
 B_tree find(int val, B_tree b) {
     if (b == NULL) return NULL;
     for (int i = 0; i < b->number_keys; i++) {
-        if (b->keys[i] == val) return b;
-        if (val < b->keys[i]) return find(val, b->children[i]);
+        if (b->keys[i] == val) return b; // Value found
+        if (val < b->keys[i]) return find(val, b->children[i]); // Go to left child
     }
-    return find(val, b->children[b->number_keys]);
+    return find(val, b->children[b->number_keys]); // Go to right child
 }
 
 void splitChild(B_tree x, int i) {
+    B_tree child = x->children[i];
     B_tree new_node = (B_tree) malloc(sizeof(B_tree_node));
     if (new_node == NULL) {
         perror("Memory allocation failed");
         exit(EXIT_FAILURE);
     }
-    B_tree child = x->children[i];
-    new_node->number_keys = m - 1;
-    child->number_keys = m;
+
     for (int j = 0; j < m - 1; j++) {
         new_node->keys[j] = child->keys[j + m];
     }
@@ -50,14 +65,209 @@ void splitChild(B_tree x, int i) {
             new_node->children[j] = child->children[j + m];
         }
     }
-    int median = child->keys[m - 1];
+    new_node->number_keys = m - 1;
+    child->number_keys = m - 1;
     for (int j = x->number_keys; j > i; j--) {
         x->keys[j] = x->keys[j - 1];
     }
-    x->keys[i] = median;
+    x->keys[i] = child->keys[m - 1];
     for (int j = x->number_keys + 1; j > i; j--) {
         x->children[j] = x->children[j - 1];
     }
     x->children[i + 1] = new_node;
     x->number_keys++;
+}
+
+void insertIncompleteNode(B_tree x, int k) {
+    int i = x->number_keys - 1;
+    if (x->children[0] == NULL) { // Leaf node
+        while (k < x->keys[i] && i >= 0) {
+            x->keys[i + 1] = x->keys[i];
+            i--;
+        }
+        x->keys[i + 1] = k;
+        x->number_keys++;
+    } else { // Internal node
+        while (k < x->keys[i] && i >= 0) {
+            i--;
+        }
+        if (x->children[i + 1]->number_keys == 2 * m + 1) {
+            splitChild(x, i + 1);
+            if (k > x->keys[i + 1]) {
+                i++;
+            }
+        }
+        insertIncompleteNode(x->children[i + 1], k);
+    }
+}
+
+void insert(B_tree x, int k) {
+    if (x->number_keys == 2 * m + 1) {
+        B_tree newRoot = create_B_tree();
+        newRoot->children[0] = x;
+        splitChild(newRoot, 0);
+        insertIncompleteNode(newRoot, k);
+    } else
+        insertIncompleteNode(x, k);
+}
+
+void borrowLeft(B_tree x, int indice) {
+    B_tree y = x->children[indice];
+    B_tree brother = x->children[indice - 1];
+    for (int i = y->number_keys; i > 0; --i) {
+        y->keys[i] = y->keys[i - 1];
+    }
+    for (int i = y->number_keys + 1; i > 0; --i) {
+        y->children[i] = y->children[i - 1];
+    }
+    y->keys[0] = x->keys[indice - 1];
+    y->children[0] = brother->children[brother->number_keys];
+    y->number_keys++;
+    x->keys[indice - 1] = brother->keys[brother->number_keys - 1];
+    brother->number_keys--;
+}
+
+void fusionLeft(B_tree x, int indice) {
+    B_tree frere = x->children[indice - 1];
+    B_tree y = x->children[indice];
+    frere->keys[frere->number_keys] = x->keys[indice - 1];
+    for (int i = 0; i < y->number_keys; i++) {
+        frere->keys[i + frere->number_keys + 1] = y->keys[i];
+        frere->children[i + frere->number_keys + 1] = y->children[i];
+    }
+    frere->children[2 * m - 1] = y->children[y->number_keys];
+    frere->number_keys = 2 * m - 1;
+    free(y);
+    for (int i = indice - 1; i < x->number_keys - 1; i++) {
+        x->keys[i] = x->keys[i + 1];
+    }
+    for (int i = indice; i < x->number_keys; i++) {
+        x->children[i] = x->children[i + 1];
+    }
+    x->number_keys--;
+}
+
+
+int sup_pred(B_tree y) {
+    if (y->children[0] == NULL) {
+        y->number_keys--;
+        return y->keys[y->number_keys];
+    }
+    if (y->children[y->number_keys]->number_keys >= m) {
+        return sup_pred(y->children[y->number_keys]);
+    }
+    if (y->children[y->number_keys - 1]->number_keys >= m) {
+        borrowLeft(y, y->number_keys);
+    } else {
+        fusionLeft(y, y->number_keys);
+    }
+    return sup_pred(y->children[y->number_keys]);
+}
+
+
+B_tree supprimerInterne(B_tree x, int k) {
+    if (x->children[0] == nullptr) {  // x is a leaf
+        int i;
+        for (i = 0; i < x->number_keys; i++) {
+            if (x->keys[i] == k) {
+                break;
+            }
+        }
+        for (int j = i; j < x->number_keys - 1; j++) {
+            x->keys[j] = x->keys[j + 1];
+        }
+        x->number_keys--;
+        if (x->number_keys == 0) {
+            return nullptr;
+        }
+        return x;
+    }
+    //x isn't a leaf
+    int i;
+    bool appartient = false;
+    for (i = 0; i < x->number_keys; i++) {
+        if (x->keys[i] == k) {
+            appartient = true;
+            break;
+        }
+        if (x->keys[i] > k) {
+            break;
+        }
+    }
+    if (appartient) {
+        if (x->children[i]->number_keys >= m) {
+            x->keys[i] = sup_pred(x->children[i]);
+            return x;
+        }
+        if (x->children[i + 1]->number_keys >= m) {
+            x->keys[i] = sup_succ(x->keys[i + 1]); // Like supp_pred (not implemented)
+            return x;
+        }
+        fusionLeft(x, i + 1);
+        return supprimerInterne(x->children[i], k);
+    }
+    if (x->children[i]->number_keys >= m) {
+        return supprimerInterne(x->children[i], k);
+    }
+    if (i > 0) {        //child i has a left brother
+        if (x->children[i - 1]->number_keys >= m) {
+            borrowLeft(x, i);
+            return supprimerInterne(x->children[i], k);
+        }
+    }
+    if (i < x->number_keys) {
+        if (x->children[i + 1]->number_keys >= m) {
+            borrowRight(x, i); //Like borrowLeft
+            return supprimerInterne(x->children[i], k);
+        }
+    }
+    if (i > 0) {
+        fusionLeft(x, i);
+        return supprimerInterne(x->children[i - 1], k);
+    }
+    if (i < m) {
+        fusionRight(x, i); //Like fusionLeft
+        return supprimerInterne(x->children[i], k);
+    }
+}
+
+B_tree delete(B_tree a, int k) {
+    if (a->number_keys == 1) {
+        if (a->children[0] != nullptr) {
+            if (a->children[0]->number_keys == m - 1 && a->children[1]->number_keys == m - 1) {
+                fusionLeft(a, 1);
+                B_tree x = a->children[0];
+                free(a);
+                return supprimerInterne(x, k);
+            }
+        }
+    }
+    return supprimerInterne(a, k);
+}
+
+int main() {
+    // Create the B-tree
+    B_tree tree = create_B_tree();
+
+    // Insert values into the B-tree
+    int values_to_insert[] = {10, 20, 5, 6, 12, 30, 7, 17};
+    int num_values = sizeof(values_to_insert) / sizeof(values_to_insert[0]);
+
+    for (int i = 0; i < num_values; i++) {
+        insert(tree, values_to_insert[i]);
+    }
+
+    // Print the B-tree structure
+    printf("B-tree structure after insertions:\n");
+    print_B_tree(tree, 0);
+
+    // Search for a value in the B-tree
+    int search_value = 12;
+    B_tree result = find(search_value, tree);
+    if (result != NULL) {
+        printf("\nValue %d found in the B-tree.\n", search_value);
+    } else {
+        printf("\nValue %d not found in the B-tree.\n", search_value);
+    }
+    return 0;
 }
